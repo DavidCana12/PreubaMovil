5d983d006277a96536174e81cc718b8133f51032
David Canache <davidcana12@hotmail.com>
2019-09-22 18:10:06 -0400

    Add project files.

---
+++ TestingExample/TestingExample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TestingExample.Models;
+
+namespace TestingExample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
